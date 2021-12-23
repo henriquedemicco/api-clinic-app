e31d8c48008bb651259f00ed8e5f8f193c266e70
Miccao <henriquedemicco@hotmail.com>
2021-12-23 10:14:08 -0300

    Alterando max_lenght do campo email

--- core/models.py
+++ core/models.py
@@ -7,11 +7,11 @@ from datetime import *
 class Paciente(models.Model):
     id_paciente = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     nome = models.CharField(max_length=150)
-    sexo = models.CharField(max_length=20)
+    sexo = models.CharField(max_length=100)
     cpf = models.CharField(max_length=100)
     celular = models.CharField(max_length=100)
     data_nascimento = models.DateField()
-    email = models.CharField(max_length=40)
+    email = models.CharField(max_length=100)
 
     def __str__(self):
         return self.nome
